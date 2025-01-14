e316e8b0c6d08737256ecd55a79a7c9d41b6f85f
cnnmon <tiffanywang@berkeley.edu>
2025-01-13 21:51:52 -0800

    Update layout.tsx

--- app/(splash)/layout.tsx
+++ app/(splash)/layout.tsx
@@ -6,10 +6,6 @@ export default function SignInLayout({ children }: { children: ReactNode }) {
     <ConvexClientProvider>
       <div className="flex justify-center items-center">
         <main className="flex grow flex-col">{children}</main>
-        <div className="fixed bottom-0 p-4 w-full">
-          <p>welcome to my equivalent of apple notes</p>
-          <p>hope you like</p>
-        </div>
       </div>
     </ConvexClientProvider>
   );
