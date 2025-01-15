289d8cf37e3a765180ce8327341972437855aace
cnnmon <tiffanywang@berkeley.edu>
2025-01-15 15:39:15 -0800

    ui

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/clerk-react";
+import { TransparencyGridIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 
 export default function SignInPage() {
@@ -13,18 +14,22 @@ export default function SignInPage() {
         signUpForceRedirectUrl={"/"}
       >
         <Button
-          className="p-2 flex flex-col"
+          className="p-1 flex flex-col items-start"
           style={{
             fontFamily: "Arial",
           }}
         >
-          <Image
-            src="/favicon.svg"
-            alt="milk"
-            width={100}
-            height={100}
-            className="z-10"
-          />
+          <div className="flex w-full justify-center">
+            <TransparencyGridIcon className="w-6 h-6 mr-[-18px]" />
+            <Image
+              src="/favicon.svg"
+              alt="milk"
+              width={100}
+              height={100}
+              className="z-10"
+            />
+          </div>
+
           <p className="text-xs">[sign in why don&apos;t you] </p>
         </Button>
       </SignInButton>
