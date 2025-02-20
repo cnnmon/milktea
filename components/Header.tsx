61d00f01878ce828551546c4cdbfe1048f5deb5c
cnnmon <tiffanywang@berkeley.edu>
2025-02-20 09:22:07 -0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export function Header({
   right?: ReactNode;
 }) {
   return (
-    <header className="fixed top-0 left-0 right-0 h-14 bg-background z-50">
+    <header className="fixed top-0 left-0 right-0 h-14 bg-background z-50 pt-20">
       <div className="p-4 absolute left-0">{left}</div>
       <div className="p-4 absolute right-0">{right}</div>
     </header>
